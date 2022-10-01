9156612a463e02afef35b78f4a18a05f358a97c8
Haze272 <studiorain27@gmail.com>
2022-10-01 12:31:00 +0300

    Заголовок и поля ввода

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './normalize.css';
 import './sass/global.scss';
 import './sass/calculator.scss';
 import './sass/elements.scss';
+import './sass/references.scss';
 
 console.log('webpack test');
