fbb8878355596fc3ab21fe8367ffdd98af1931b1
Haze272 <studiorain27@gmail.com>
2022-10-01 19:12:50 +0300

    Работающий элемент формы

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import './sass/calculator.scss';
 import './sass/elements.scss';
 import './sass/references.scss';
 
+let carCostVal = document.getElementById('car-cost-slider');
+let rangeValue = () => {
+    let newValue = carCostVal.value;
+    document.getElementById('car-cost-input').value = newValue;
+}
+
+carCostVal.addEventListener("input", rangeValue);
+
+const input = document.getElementById('car-cost-input');
+input.onchange = (e) => {
+    const { value } = e.target;
+    console.log(value);
+    document.querySelector("input[id=car-cost-slider]").value = value;
+};
+
+console.log(document.getElementById('car-cost-input').classList);
+/*
 var elem = document.querySelector('input[type="range"]');
 
 var rangeValue = function(){
@@ -19,3 +36,4 @@ input.onchange = (e) => {
     console.log(value);
     document.querySelector("input[id=govno]").value = value;
 };
+*/
