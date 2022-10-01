99f89f6e07647c5bf9a8012325da92340f58999e
Haze272 <studiorain27@gmail.com>
2022-10-01 18:30:05 +0300

    Работающий двусторонний ползунок

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@ import './sass/calculator.scss';
 import './sass/elements.scss';
 import './sass/references.scss';
 
-console.log('webpack test');
+var elem = document.querySelector('input[type="range"]');
+
+var rangeValue = function(){
+  var newValue = elem.value;
+  document.getElementById('inputy').value = newValue;
+}
+
+elem.addEventListener("input", rangeValue);
+
+const input = document.querySelector("input[id=inputy]");
+input.onchange = (e) => {
+    const { value } = e.target;
+    console.log(value);
+    document.querySelector("input[id=govno]").value = value;
+};
