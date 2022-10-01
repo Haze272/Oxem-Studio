949c8aca5d979a9acd25f3948bf2767363933aec
Haze272 <studiorain27@gmail.com>
2022-10-01 22:16:57 +0300

    Ограничители

--- src/index.js
+++ src/index.js
@@ -4,8 +4,18 @@ import './sass/calculator.scss';
 import './sass/elements.scss';
 import './sass/references.scss';
 
-const cbox = document.querySelectorAll(".biba");
 
+function jopa(element)
+{
+    if (element.value < 1000000) {
+        element.value = 1000000;
+    }
+    else if (element.value > 6000000) {
+        element.value = 6000000;
+    }
+}
+
+const cbox = document.querySelectorAll(".biba");
 for (let i = 0; i < cbox.length; i++) {
     cbox[i].addEventListener("input", () => {
         let newValue = cbox[i].value;
@@ -14,10 +24,23 @@ for (let i = 0; i < cbox.length; i++) {
 }
 
 const xbox = document.querySelectorAll(".boba")
-
 for (let j = 0; j < xbox.length; j++) {
     xbox[j].oninput = (e) => {
         const { value } = e.target;
         document.querySelector('input[name=' + xbox[j].name + '].biba').value = value;
     }
+}
+
+const zbox = document.getElementById("initial-payment-input");
+zbox.oninput = (e) => {
+    const { value } = e.target;
+    document.getElementById('initial-payment-slider').value = value / document.getElementById('car-cost-input').value * 100;
+    document.getElementById('initial-payment-percent').innerHTML = (value / document.getElementById('car-cost-input').value * 100) + "%";
+}
+
+const ybox = document.getElementById("initial-payment-slider");
+ybox.oninput = (e) => {
+    const { value } = e.target;
+    document.getElementById('initial-payment-percent').innerHTML = value + "%";
+    document.getElementById('initial-payment-input').value = value * document.getElementById('car-cost-input').value / 100;
 }
