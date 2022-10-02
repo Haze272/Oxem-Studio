4244d2930d36005e26f6fa6e29d837aed74409b4
Haze272 <studiorain27@gmail.com>
2022-10-02 15:23:02 +0300

    Реактивный калькуятор

--- src/index.js
+++ src/index.js
@@ -7,6 +7,22 @@ import './sass/references.scss';
 
 const carCost = document.getElementById('car-cost-input');
 const initialPayment = document.getElementById('initial-payment-input');
+const leasePeriod = document.getElementById('lease-period-value');
+const contractAmount = document.getElementById('lease-contract-value');
+const monthlyFee = document.getElementById('monthly-fee-value');
+
+function calculateMonthlyFee() {
+    document.getElementById('monthly-fee-value').innerHTML = Math.trunc((carCost.value - initialPayment.value) * ((0.035 * Math.pow((1 + 0.035), leasePeriod.value)) / (Math.pow((1 + 0.035), document.getElementById('lease-period-value').value) - 1)));
+    console.log('monthly fee has been calculated!');
+}
+
+function calculateInitialPayment() {
+    initialPayment.value = carCost.value * document.getElementById("initial-payment-slider").value / 100;
+}
+
+function calculateContractAmount() {
+    contractAmount.innerHTML = parseInt(initialPayment.value) + (leasePeriod.value * parseInt(monthlyFee.innerHTML));
+}
 
 const cbox = document.querySelectorAll(".biba");
 for (let i = 0; i < cbox.length; i++) {
@@ -14,8 +30,10 @@ for (let i = 0; i < cbox.length; i++) {
         let newValue = cbox[i].value;
         document.querySelector('input[name=' + cbox[i].name + '].boba').value = newValue;
         if (cbox[i].name == "super-input1") {
-            initialPayment.value = newValue * document.getElementById("initial-payment-slider").value / 100;
+            calculateInitialPayment();
         }
+        calculateMonthlyFee();
+        calculateContractAmount();
     });
 }
 
@@ -25,8 +43,10 @@ for (let j = 0; j < xbox.length; j++) {
         const { value } = e.target;
         document.querySelector('input[name=' + xbox[j].name + '].biba').value = value;
         if (xbox[j].name == "super-input1") {
-            initialPayment.value = value * document.getElementById("initial-payment-slider").value / 100;
+            calculateInitialPayment();
         }
+        calculateMonthlyFee();
+        calculateContractAmount();
     }
 }
 
@@ -35,11 +55,15 @@ zbox.oninput = (e) => {
     const { value } = e.target;
     document.getElementById('initial-payment-slider').value = value / carCost.value * 100;
     document.getElementById('initial-payment-percent').innerHTML = (value / carCost.value * 100) + "%";
+    calculateMonthlyFee();
+    calculateContractAmount();
 }
 
 const ybox = document.getElementById("initial-payment-slider");
 ybox.oninput = (e) => {
     const { value } = e.target;
     document.getElementById('initial-payment-percent').innerHTML = value + "%";
-    initialPayment.value = value * carCost.value / 100;
+    calculateInitialPayment();
+    calculateMonthlyFee();
+    calculateContractAmount();
 }
