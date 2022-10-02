9432ad813f63ef584ac1964ea8fc08b09bfdf07c
Haze272 <studiorain27@gmail.com>
2022-10-02 14:46:06 +0300

    Привязка стоимости к первоначальному взносу

--- src/index.js
+++ src/index.js
@@ -5,21 +5,17 @@ import './sass/elements.scss';
 import './sass/references.scss';
 
 
-function jopa(element)
-{
-    if (element.value < 1000000) {
-        element.value = 1000000;
-    }
-    else if (element.value > 6000000) {
-        element.value = 6000000;
-    }
-}
+const carCost = document.getElementById('car-cost-input');
+const initialPayment = document.getElementById('initial-payment-input');
 
 const cbox = document.querySelectorAll(".biba");
 for (let i = 0; i < cbox.length; i++) {
     cbox[i].addEventListener("input", () => {
         let newValue = cbox[i].value;
         document.querySelector('input[name=' + cbox[i].name + '].boba').value = newValue;
+        if (cbox[i].name == "super-input1") {
+            initialPayment.value = newValue * document.getElementById("initial-payment-slider").value / 100;
+        }
     });
 }
 
@@ -28,19 +24,22 @@ for (let j = 0; j < xbox.length; j++) {
     xbox[j].oninput = (e) => {
         const { value } = e.target;
         document.querySelector('input[name=' + xbox[j].name + '].biba').value = value;
+        if (xbox[j].name == "super-input1") {
+            initialPayment.value = value * document.getElementById("initial-payment-slider").value / 100;
+        }
     }
 }
 
 const zbox = document.getElementById("initial-payment-input");
 zbox.oninput = (e) => {
     const { value } = e.target;
-    document.getElementById('initial-payment-slider').value = value / document.getElementById('car-cost-input').value * 100;
-    document.getElementById('initial-payment-percent').innerHTML = (value / document.getElementById('car-cost-input').value * 100) + "%";
+    document.getElementById('initial-payment-slider').value = value / carCost.value * 100;
+    document.getElementById('initial-payment-percent').innerHTML = (value / carCost.value * 100) + "%";
 }
 
 const ybox = document.getElementById("initial-payment-slider");
 ybox.oninput = (e) => {
     const { value } = e.target;
     document.getElementById('initial-payment-percent').innerHTML = value + "%";
-    document.getElementById('initial-payment-input').value = value * document.getElementById('car-cost-input').value / 100;
+    initialPayment.value = value * carCost.value / 100;
 }
